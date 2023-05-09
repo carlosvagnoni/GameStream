bf03f9f850f5cb16d5c8d04bd2394170eddd5617
Carlos Vagnoni <carlosvagnoni11@gmail.com>
2023-05-09 19:06:16 -0400

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by user239477 on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
